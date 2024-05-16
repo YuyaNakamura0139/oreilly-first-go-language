7b9eb4ffc9a81afd88e0ddab720826ab3e391521
YuyaNakamura0139 <mtk371@icloud.com>
2024-05-16 09:08:33 +0900

    advance chapter4

---
+++ example/ch04/ex3.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	n := r.Intn(10)
+	if n == 0 {
+		fmt.Println("少し小さすぎます:", n)
+	} else if n > 5 {
+		fmt.Println("大きすぎます:", n)
+	} else {
+		fmt.Println("いい感じの数字です:", n)
+	}
+}
